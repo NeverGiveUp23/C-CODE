4c13fc16d0263094958f1a87c6fe31a1367453d4
felix <felixvjr0@gmail.com>
2024-11-01 16:05:33 -0400

    mortgage calc

--- mortgageCalc/mortgage.c
+++ mortgageCalc/mortgage.c
@@ -21,9 +21,16 @@ int main(){
     interestRate /= 1200;
     numOfPayments *= 12;
 
+    // within the 'pow' operator the second variable is the number in the power ^.
+    // so in this example it would be 1 + 5^n - as an example 'n' being numOfPanyments
     monthlyPayments = loanAmount * (interestRate * pow(1 + interestRate, numOfPayments)) / (pow(1 + interestRate, numOfPayments) - 1);
 
 
     printf("Monthly payments: %.2f\n", monthlyPayments);
+
+
+    double number = pow(5 + 1, 2);
+
+    printf("%.2lf\n", number);
     return 0;
 }
