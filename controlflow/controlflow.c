62fcc10c26106c9b38bf112e877d24fba496e8d9
felix <felixvjr0@gmail.com>
2024-11-04 08:03:41 -0500

    structs and pointers, enums and typedef

---
+++ controlflow/controlflow.c
@@ -0,0 +1,9 @@
+//
+// Created by Felix Vargas Jr on 11/4/24.
+//
+int main() {
+
+
+
+    return 0;
+}
--- loops/loops.c
+++ loops/loops.c
@@ -7,7 +7,18 @@
 
 int main(){
 
+int n, number, triangleNumber;
 
+
+    printf("Can you pick a number to triangulate: ");
+    scanf("%i", &number);
+
+    triangleNumber = 0;
+    for(n = 1; n <= number; n++){
+        triangleNumber += n;
+    }
+
+    printf("Triangular number %i is %i\n", number, triangleNumber);
 
 
     return 0;
