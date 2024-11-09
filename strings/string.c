1be45c7f7e1bd7a7f280e5bbfca7faa9d7af9828
felix <felixvjr0@gmail.com>
2024-11-08 19:40:14 -0500

    Strings

--- strings/string.c
+++ strings/string.c
@@ -28,9 +28,16 @@ char string[] = {"Hello!"};
 
 const int MONTHS = 12; // turns it into a read only value and lets you declare a type
 
+// we can use string libaries
+char str1[10];
+
 int main() {
   char s[100];
   int i;
+
+  // strcpy will copy the string into the variable but will not check the length
+  // strncpy will do the same but will ask for a 3rd parameter of the fixed length that you need
+  strncpy(str1, string, 10); // if string in the second param is bigger than 10, str1 will only copy the first 10 chars
 
   strcpy(s, "Hello");
   char t[100];
@@ -46,6 +53,8 @@ int main() {
       };
   }
 }
+
+printf("Length of the string is: %d\n", strlen(s));
 
 return 0;
 }
