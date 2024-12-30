52a65d97efb1bc3aef87c2d4fc7d4ada4ddd333d
felix <felixvjr0@gmail.com>
2024-12-29 23:53:23 -0500

    Sum numbers, putting into function and using pointers with structs

--- advanced/varlengtharr.c
+++ advanced/varlengtharr.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+struct arrDef {
+    int arrSize;
+};
 
 int totalSum(){
-    int arrSize = 0; // for the user input and where to store it
-    printf("Enter a number: ");
-    scanf("%i", &arrSize); // storing user input for length of the array
+    int size = 0;// for the user input and where to store it
+    struct arrDef val = {size}; // defining the value for the arrSize in the struct
+    struct arrDef* val_p = &val; // setting up a pointer to point to the struct
 
-    int arr[arrSize]; // putting the length of the array into the actual array
+    printf("Enter a number: ");
+    scanf("%i", &val_p->arrSize); // storing user input for length of the array -> using the pointer to get the address of arrSize
+
+    int arr[val_p->arrSize]; // putting the length of the array into the actual array -> using the pointer to use the value
 
     bool c = true; // for the while loop condition
     int count = 0; // count to know when to exit the while loop
 
     while(c){ // while loop since we need to get each input for the N size of the array
 
-        for(int i = 0; i < arrSize; ++i){ // starting the loop
+        for(int i = 0; i < val_p->arrSize; ++i){ // starting the loop -> pointing to the value
             printf("Number %i: ", count);
             scanf("%d", &arr[i]); // inputting the number from user input on the current element i
             count++; // increment count to eventually match the arrSize to know when to exit the loop
         }
 
-        if(count == arrSize) // check if match
+        if(count == val_p->arrSize) // check if match
             c = false; // if match, exit the while loop by changing to false
     }
 
     int sum = 0;
 
-    for(int j = 0; j < arrSize; ++j){
+    for(int j = 0; j < val_p->arrSize; ++j){
         sum += arr[j]; // going through the elements values and adding
     }
 
