4ab44455657caf62efc74e9b203cc3060f659641
felix <felixvjr0@gmail.com>
2024-12-29 23:03:50 -0500

    Sum numbers with user input for array length

--- advanced/varlengtharr.c
+++ advanced/varlengtharr.c
@@ -3,31 +3,31 @@
 
 int main() {
 
-    int arrSize = 0;
+    int arrSize = 0; // for the user input and where to store it
     printf("Enter a number: ");
-    scanf("%i", &arrSize);
+    scanf("%i", &arrSize); // storing user input for length of the array
 
-    int arr[arrSize];
+    int arr[arrSize]; // putting the length of the array into the actual array
 
-    bool c = true;
-    int count = 0;
+    bool c = true; // for the while loop condition
+    int count = 0; // count to know when to exit the while loop
 
-    while(c){
+    while(c){ // while loop since we need to get each input for the N size of the array
 
-        for(int i = 0; i < arrSize; ++i){
+        for(int i = 0; i < arrSize; ++i){ // starting the loop
             printf("Number %i: ", count);
-            scanf("%d", &arr[i]);
-            count++;
+            scanf("%d", &arr[i]); // inputting the number from user input on the current element i
+            count++; // increment count to eventually match the arrSize to know when to exit the loop
         }
 
-        if(count == arrSize)
-            c = false;
+        if(count == arrSize) // check if match
+            c = false; // if match, exit the while loop by changing to false
     }
 
     int sum = 0;
 
     for(int j = 0; j < arrSize; ++j){
-        sum += arr[j];
+        sum += arr[j]; // going through the elements values and adding
     }
 
     printf("%d", sum);
