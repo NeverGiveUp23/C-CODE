8722cdab16bb68a031356444d104ededac8e004e
felix <felixvjr0@gmail.com>
2024-12-29 22:44:28 -0500

    Sum numbers with user input for array length

--- advanced/varlengtharr.c
+++ advanced/varlengtharr.c
@@ -1,41 +1,36 @@
-#include <stddef.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 
-  size_t size = 0;
-  printf("Enter the numbedr you want to store: ");
-  scanf("%zd", &size);
+    int arrSize = 0;
+    printf("Enter a number: ");
+    scanf("%i", &arrSize);
 
-  float values[size];
+    int arr[arrSize];
 
-  int rows = 0, cols = 0;
-  printf("Enter the number of rows: ");
-  scanf("%i", &rows);
+    bool c = true;
+    int count = 0;
 
-  printf("Enter number of columns: ");
-  scanf("%i", &cols);
+    while(c){
 
-  int table[rows][cols];
-  int tot = 0;
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < cols; ++j) {
-      tot += table[i][j];
+        for(int i = 0; i < arrSize; ++i){
+            printf("Number %i: ", count);
+            scanf("%d", &arr[i]);
+            count++;
+        }
+
+        if(count == arrSize)
+            c = false;
     }
-  }
-  printf("%i", tot);
 
-  int total = 0;
-  printf("Enter a size of the array you want: ");
-  scanf("%i", &total);
-  printf("You entered %i\n", total);
+    int sum = 0;
 
-  int arr[total];
-  printf("Please enter %i numbers: ", total);
-  int i;
-  for (i = 0; i < total; ++i) {
-    scanf("%i", );
-  }
+    for(int j = 0; j < arrSize; ++j){
+        sum += arr[j];
+    }
+
+    printf("%d", sum);
 
   return 0;
 }
